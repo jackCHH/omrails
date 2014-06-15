aae4180d2a1fc4ca56d43e00c851adb352233ace
Jack Hou <jack.hou@Live.com>
2014-06-14 22:28:48 -0700

    Added Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Omrails::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #in production, the :host should be the actual host
+  config.action_mailer.default_url_options = { host: 'localhost:3000'}
 end
